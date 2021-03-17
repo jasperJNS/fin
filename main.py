bfe702b66d3200f4bff5623fd45e251e13a7a398
jasperSha <jaysha54@gmail.com>
2021-03-17 11:01:04 -0700

    defined fin object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import td
 
+import pickle
 import pprint as pp
 import pandas as pd
 pd.set_option('display.max_columns', None)
@@ -7,21 +8,63 @@ pd.set_option('display.max_columns', None)
 from login import login
 from options_chain import get_options_chain
 
+class Fin:
+    def __init__(self, session, opt_params):
+        self.session = session
+        self.opt_params = opt_params
 
-# Get session object
+        self.chain = None
+
+    def set_chain(self):
+        self.chain = pd.DataFrame(get_options_chain(self.session, self.opt_params))
+
+    def get_chain(self):
+        return self.chain
+    
+    def set_opt_params(self, opt_params):
+        self.opt_params = opt_params
+    
+    def write_chain(self, filename):
+        '''
+            writes to ./data
+        '''
+        fname = './data/' + filename + '.pkl'
+        self.chain.to_pickle(fname)
+    
+    def read_chain(self, filename):
+        '''
+            reads from ./data
+        '''
+        fname = './data/' + filename + '.pkl'
+        self.chain = pd.read_pickle(fname)
+
 session = login()
 
-nio = session.get_quotes(instruments=['NIO'])
-pp.pprint(nio, width=1)
 opt_params = {
     'symbol': 'NIO',
     'opt_range': 'all',
     'exp_month': 'may'
 }
 
-chain = get_options_chain(session, opt_params)
+nio = Fin(session, opt_params)
+
+# set from session object
+nio.set_chain()
 
 
-chain_df = pd.DataFrame(chain)
+chain_df = nio.get_chain()
+nio.write_chain('nio_chain')
 
-print(chain_df.head())
+
+# write to file
+# chain_df.to_pickle('./nio_chain.pkl')
+
+# # read file
+# chain_df = pd.read_pickle('./nio_chain.pkl')
+
+
+
+calls = chain_df['callExpDateMap']
+strike = '20.0'
+pp.pprint(calls.iloc[0][strike], indent=4)
+

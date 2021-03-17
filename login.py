51dea7556db42d9a26439d35f8b639a552c23e34
jasperSha <jaysha54@gmail.com>
2021-03-17 09:21:48 -0700

    added login session

---
+++ login.py
@@ -0,0 +1,17 @@
+# import td-ameritrade-python-api as td
+from config.config import CLIENT_ID, ACCOUNT, PASSWORD, REDIRECT_URI, CREDENTIALS_PATH
+import td
+from td.client import TDClient
+
+def login():
+    # Create a new session, credentials path is required.
+    TDSession = TDClient(
+        client_id=CLIENT_ID,
+        redirect_uri=REDIRECT_URI,
+        credentials_path=CREDENTIALS_PATH
+    )
+
+    # Login to the session
+    TDSession.login()
+
+    return TDSession

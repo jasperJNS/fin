f59e7774464ba7dfa354e78980dc87947dda2745
jasperSha <jaysha54@gmail.com>
2021-03-20 19:41:08 -0700

    collapsed helper functions into object

--- Fin.py
+++ Fin.py
@@ -1,34 +1,36 @@
 import pickle
+import time
+from datetime import datetime
 import pandas as pd
-from options_chain import get_options_chain
 
 class Fin:
-    def __init__(self, session, opt_params):
+    def __init__(self, session=None, opt_params=None):
+        '''
+            sets chain on init with session and params objects
+        '''
         self.session = session
         self.opt_params = opt_params
 
-        self.chain = None
-
-    def set_chain(self):
-        self.chain = pd.DataFrame(get_options_chain(self.session, self.opt_params))
-
+        if session is not None:
+            self.chain = session.get_options_chain(option_chain=opt_params)
+    
     def get_chain(self):
         return self.chain
     
     def set_opt_params(self, opt_params):
         self.opt_params = opt_params
     
-    def write_file(self, filename):
+
+    def write_json(self, filename):
         '''
             writes to ./data
         '''
-        fname = './data/' + filename + '.pkl'
-        self.chain.to_pickle(fname)
+        fname = './data/' + filename + '.json'
+        self.chain.to_json(fname, orient='records', indent=2)
     
-    def read_file(self, filename):
+    def read_json(self, filename):
         '''
-            reads from ./data
-            doesn't require active session
+            reads json into pandas dataframe
         '''
-        fname = './data/' + filename + '.pkl'
-        self.chain = pd.read_pickle(fname)
+        fname = './data/' + filename + '.json'
+        self.chain = pd.read_json(fname, orient='records')
--- main.py
+++ main.py
@@ -4,35 +4,94 @@ pd.set_option('display.max_columns', None)
 
 from login import login
 from Fin import Fin
+from CallPut import CallPut
+
+def main_test():
+    # session = login()
+
+    # nio_params = {
+    #     'symbol': 'NIO',
+    #     'opt_range': 'all',
+    #     'exp_month': 'may'  #months up to, but NOT including {exp_month}
+    # }
+
+    # nio = Fin(session, nio_params)
+
+    # # set from session object
+    # nio.set_chain()
+    # nio.write_file('nio_2021319')
 
 
-# session = login()
+    # temp just for reading file
+    nio = Fin()
+    nio.read_file('nio_all_dates')
 
-# nio_params = {
-#     'symbol': 'NIO',
-#     'opt_range': 'all',
-#     'exp_month': 'may'
-# }
-
-# nio = Fin(session, nio_params)
-
-# # set from session object
-# nio.set_chain()
-# nio.write_file('nio_chain')
+    print('\n'*5)
 
 
-# temp just for reading file
-nio = Fin(None, None)
-nio.read_file('nio_chain')
+    chain = nio.get_chain()
+    chain_df = pd.DataFrame(chain)
+    print(chain_df.head())
+    chain_df.to_json('nio_all_dates.json', orient='records', indent=2)
+
+
+def json_test():
+    nio = Fin()
+    nio.read_json('nio_all_dates')
+    chain = nio.get_chain()
+
+    print("cols: ", chain.columns)
+
+    #these are universal across all dates
+    symbol = chain['symbol'].iloc[0]
+    underlying = chain['underlyingPrice'].iloc[0]
+    vol = chain['volatility'].iloc[0]
+    totalNumContracts = chain['numberOfContracts'].iloc[0]
+
+    '''
+        so each row in callexdatemap or putexpdatemap is date indexed
+
+        so let's unwrap, each row will be its own dataframe, records style
+
+        then overall data is a list of dates, then a dictionary of dates to their respective dfs
+
+
+    '''
+    print(chain.columns, chain.shape)
+
+    callsMap = chain['callExpDateMap']
+
+    #using the first strike of each expiry date chain to get the date of the chain
+    dates = []
+    calls = []
+    for idx, callMap in enumerate(callsMap):
+        for strikes in list(callsMap[idx].keys()):
+            strike = strikes
+            call = CallPut(dict(callsMap[idx][strike][0].items()))
+            
+            calls.append(call)
+            
+        #use last call in chain to get date
+        date = call.get('expirationDate')
+        dates.append(date)
+
+    puts = []
+    putsMap = chain['putExpDateMap']
+    for idx, putMap in enumerate(putsMap):
+        for strikes in list(putsMap[idx].keys()):
+            strike = strikes
+            put = CallPut(dict(putsMap[idx][strike][0].items()))
+            
+            puts.append(put)
+
+    for k, v in puts[0].items():
+        print(k, v)
+    for k, v in calls[0].items():
+        print(k, v)
 
 
 
-chain_df = nio.get_chain()
-
-calls = chain_df['callExpDateMap']
 
 
-
-strike = '20.0'
-pp.pprint(calls.iloc[0][strike], indent=4)
-
+if __name__ == '__main__':
+    json_test()
